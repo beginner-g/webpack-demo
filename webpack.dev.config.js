02368e2f63ead8d2c34586bedc84627019f33c32
beginner-g <969925523@qq.com>
2017-07-06 17:09:05 +0800

    flex

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,39 @@
-/*
-* @Author: anchen
-* @Date:   2017-07-06 11:48:21
-* @Last Modified by:   anchen
-* @Last Modified time: 2017-07-06 11:48:52
-*/
-
-'use strict';
+const path = require('path');
+const webpack =require('webpack')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+module.exports={
+    entry:"./src/index.js",
+    output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve:{
+    extensions:['.js','.css','.jsx','.json']
+  },
+  devServer: {
+      compress: true,
+      port: 3000,
+      hot: true
+    },
+  devtool:'source-map',
+  module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, use: "babel-loader" },
+        {
+        test: /\.css$/,use: ['style-loader','css-loader','postcss-loader']
+        },
+        {test: /\.(jpg?g|png)$/,use:'file-loader'}
+      ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'public/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:3000'
+        })
+    ]
+}
